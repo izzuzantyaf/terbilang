a9ab94793908a31a1d49472edac6617fbb59678f
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-04-17 09:48:44 +0700

    initial custom setup

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="homepage">
+      <Head>
+        <title>Terbilang</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    </div>
+  )
+}
